6b0ff5debf246d02745006cfdaa9612618581e4f
Prabhat <prabhatkumarpdy2016@gmail.com>
2025-05-25 01:05:11 +0530

    TerraForm-Notes

--- ec2-instance-cration.tf
+++
@@ -1,9 +0,0 @@
-# Configure AWS provider
-provider "aws" {
-  region = "us-east-1"  # You can change the region as needed
-}
-
-# Create EC2 instance
-resource "aws_instance" "test" {
-  ami           = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2 AMI (us-east-1)
-  instance_type = "t2.micro"               # Instance type
